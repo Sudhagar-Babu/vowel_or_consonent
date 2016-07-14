44d001826a87a3f0240d4d7e03901e615f83a6ff
Sudhagar-Babu <sudhagar.it13@bitsathy.ac.in>
2016-07-14 10:32:23 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h> 
+int main() 
+{
+char a; 
+clrscr();   
+scanf("%c",a);    
+if(a=='a'||a=='A'||a=='e'||a=='E'||a=='i'||a=='I'||a=='o'||a=='O'||a=='u'||a=='U')
+{
+printf("vowel");    
+}   
+else
+  printf("consonent");
+ getch(); 
+ return 0; 
+}
